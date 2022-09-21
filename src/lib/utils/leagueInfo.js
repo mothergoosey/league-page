f9da7a9192a0f1e362b0099b0ee1e96c35d582a2
mothergoosey <110045731+mothergoosey@users.noreply.github.com>
2022-09-21 09:56:16 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "781880478381768704"; // your league ID
 export const leagueName = "Diners, Dynasties and Dives"; // your league name
 export const dues = 75; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
